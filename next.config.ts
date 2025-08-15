35ff9c0b34f76c2d2488edf43a4461df80fd9c2e
itcbrasil-tecnologia <tecnologia.itcbrasil@gmail.com>
2025-08-15 15:48:45 -0300

    componente next-pwa

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,14 @@
 import type { NextConfig } from "next";
-import withPWA from "next-pwa";
+// A importação agora vem da nova biblioteca
+import withPWAInit from "@ducanh2912/next-pwa";
 
-// A configuração base do Next.js. Atualmente, não temos opções customizadas.
-const nextConfig: NextConfig = {};
-
-// Envolve a configuração do Next.js com as configurações do PWA
-// utilizando a sintaxe de função de ordem superior (higher-order function)
-export default withPWA({
+const withPWA = withPWAInit({
   dest: "public",
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === "development",
-})(nextConfig);
+});
+
+const nextConfig: NextConfig = {};
+
+export default withPWA(nextConfig);
